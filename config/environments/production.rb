d3aaba00c2065ca82a9723302e187081d2bebf1a
Perry Ahern <pahern@gmail.com>
2017-07-24 22:51:28 -0400

    Trying to fix previous commit attempt after checkpoint 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Configure Devise for user authentication
+  config.action_mailer.default_url_options = { host: 'agile-wildwood-20495.herokuapp.com/' }
 end
